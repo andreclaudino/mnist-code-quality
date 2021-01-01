325a2b2a2ba9a2ea82a732c7c840b5839acb0733
André Claudino <claudino@d2x.com.br>
2021-01-01 16:58:50 -0300

    Start a python prioject

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mnist',
+    version='0.0.1',
+    packages=['mnist', 'mnist.persistence'],
+    url='',
+    license='',
+    author='André Claudino',
+    author_email='',
+    description='',
+    install_requires=[
+        "tensorflow-gpu==2.4.0"
+    ]
+)
